6f6b9e642061a371b1a223291132c8a4649f84c8
“agustinalonsocantoli” <agustinalonsocantoli@gmail.com>
2026-03-23 12:58:50 +0100

    add seo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -110,9 +110,14 @@ export default function Footer() {
 
       {/* Copyright */}
       <div className="mt-8 border-t border-[#878787] pt-8 text-center">
-        <p className="font-body text-sm leading-5 text-[#99a1af]">
-          © 2026 NEXO CrossFit. Todos los derechos reservados.
-        </p>
+        <div>
+          <p className="font-body text-sm leading-5 text-[#99a1af]">
+            © 2026 NEXO CrossFit. Todos los derechos reservados.
+          </p>
+          <p className="font-body text-xs leading-5 text-[#99a1af]">
+            SEO by <Link href="https://snwebsolution.com/es/">SN websolution</Link>
+          </p>
+        </div>
       </div>
     </footer>
   );
