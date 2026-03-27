681471d5f5149fdb572f8e6663b0fc0fd2795225
“agustinalonsocantoli” <agustinalonsocantoli@gmail.com>
2026-03-27 17:31:08 +0100

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/**": ["./generated/prisma/**/*"],
+  },
   async redirects() {
     return [
       // =============================================
